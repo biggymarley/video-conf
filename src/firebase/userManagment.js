d742741a6700778e7a2c4a4c1fa36d838948b3d6
afaragi <afaragi@zobristinc.com>
2024-04-28 05:56:58 +0100

    fixingg bug

--- src/firebase/userManagment.js
+++ src/firebase/userManagment.js
@@ -37,10 +37,48 @@ export const loginUser = async (email, password, saveToken, setAppLoading) => {
     );
   }
 };
+const colors = [
+  { name: "Slate", value: "#64748b" },
+  { name: "Gray", value: "#6b7280" },
+  { name: "Zinc", value: "#71717a" },
+  { name: "Neutral", value: "#737373" },
+  { name: "Stone", value: "#78716c" },
+  { name: "Red", value: "#ef4444" },
+  { name: "Orange", value: "#f97316" },
+  { name: "Amber", value: "#f59e0b" },
+  { name: "Yellow", value: "#eab308" },
+  { name: "Lime", value: "#84cc16" },
+  { name: "Green", value: "#22c55e" },
+  { name: "Emerald", value: "#10b981" },
+  { name: "Teal", value: "#14b8a6" },
+  { name: "Cyan", value: "#06b6d4" },
+  { name: "Sky", value: "#0ea5e9" },
+  { name: "Blue", value: "#3b82f6" },
+  { name: "Indigo", value: "#6366f1" },
+  { name: "Violet", value: "#8b5cf6" },
+  { name: "Purple", value: "#a855f7" },
+  { name: "Fuchsia", value: "#d946ef" },
+  { name: "Pink", value: "#ec4899" },
+  { name: "Rose", value: "#f43f5e" },
+];
 
-export const createUser = async (email, password, userName, setAppLoading, saveToken) => {
+export const createUser = async (
+  email,
+  password,
+  userName,
+  setAppLoading,
+  saveToken
+) => {
   try {
     setAppLoading(true);
+    const getRandomColor = () => {
+      const filteredColors = colors.filter(
+        (color) => color.value === "#3b82f6"
+      );
+      const randomIndex = Math.floor(Math.random() * filteredColors.length);
+      const selectedColor = filteredColors[randomIndex];
+      return `text-${selectedColor.name.toLowerCase()}-500`;
+    };
     const userCredential = await createUserWithEmailAndPassword(
       auth,
       email,
@@ -55,6 +93,7 @@ export const createUser = async (email, password, userName, setAppLoading, saveT
         uid: userCredential.user.uid,
         userName: userName,
         email: email,
+        color: getRandomColor(),
       };
       await setDoc(doc(db, "users", userCredential.user.uid), user);
     }
